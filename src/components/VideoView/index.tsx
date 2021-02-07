ac69c9ae95e49be65b901e82e8d1f2350911a967
Arnas Puidokas <arpu@devlabers.com>
2021-02-07 21:27:09 +0200

    feat: add grid, video view

--- src/components/VideoView/index.tsx
+++ src/components/VideoView/index.tsx
@@ -1,4 +1,23 @@
 import React, { FC } from 'react';
 import '../../tailwind.output.css';
+import ReactPlayer from 'react-player';
+import './index.css';
 
-export const VideoView: FC = () => <>hey</>;
+export const VideoView: FC = () => (
+    <>
+        <div className="relative player-wrapper">
+            <ReactPlayer
+                className="absolute top-0 left-0"
+                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                width="100%"
+                height="100%"
+            />
+        </div>
+        <h1>Title</h1>
+        <p>
+            One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book it has?
+        </p>
+    </>
+);
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -6,6 +6,47 @@ import '../../tailwind.output.css';
 export const Main: FC = () => (
     <>
         <SearchBar />
-        <VideoView />
+        <div className="grid grid-cols-1 lg:grid-cols-3 p-5">
+            <div className="col-span-2">
+                <VideoView />
+            </div>
+            <div className="col-span-1 lg:ml-5">
+                One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown
+                printer took a galley of type and scrambled it to make a type specimen book it has?
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div className="mt-8">
+                    One of many rows: What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when
+                    an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                </div>
+                <div>One of many rows</div>
+                <div>One of many rows</div>
+            </div>
+        </div>
     </>
 );
